d94dfe265c1337d9e7642ff0a4732063fca8f8c1
Henrison-tao <henrisontao@gmail.com>
2017-10-26 13:12:08 +0800

    add func

--- getFreeCoin_remove.js
+++ getFreeCoin_remove.js
@@ -6,9 +6,22 @@ var rows = $('#freecoins_table > tbody > tr');
     });
 paidRows.remove();
 
+//清除所有跳出的modal
+var mes=$('.messi');
+var mesm=$('.messi-modal');
+mes.remove();
+mesm.remove();
+var rows = $('#freecoins_table > tbody > tr');
+
+   var paidRows = rows.filter(function () {
+        return $(this).children(':nth-child(3)').text() !== 'ready';
+    });
+paidRows.remove();
+
 //點擊所有getCoin按鈕
 $('.clGetFreeCoins').each(function (index) {
     setTimeout(function () {
         $(this).click();
     }.bind(this), index * 1E3);
 });
+
